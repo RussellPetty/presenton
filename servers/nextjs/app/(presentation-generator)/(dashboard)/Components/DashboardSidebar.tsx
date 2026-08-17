50d10089b9dd578797840c269bb06f963d5fe0fe
ShivaRajBadu <shivabadu5@gmail.com>
2026-08-17 16:03:00 +0545

    Open settings in the current tab

--- servers/nextjs/app/(presentation-generator)/(dashboard)/Components/DashboardSidebar.tsx
+++ servers/nextjs/app/(presentation-generator)/(dashboard)/Components/DashboardSidebar.tsx
@@ -100,7 +100,7 @@ const DashboardSidebar = () => {
             <div className="border-t border-[#E1E1E5] pt-5 font-syne">
                   <Link
                     href="/settings"
-                    target="_blank"
+                  
                     className="flex flex-col items-center gap-2 transition-colors"
                 >
                     <Settings className="h-4 w-4" />
